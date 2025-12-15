9d784aaf572ba573720a7d94939062890353d90d
Manuel Chekwubechukwu <emmanuelchekwubechukwu22@gmail.com>
2025-12-15 16:48:46 +0100

    Refactor age calculation and comment jagged array
    Commented out the jagged array initialization and added code to calculate the sum of student ages.

--- Mini-DSA-project.cs
+++ Mini-DSA-project.cs
@@ -16,7 +16,7 @@ public class JaggedArrays
     public static void Main()
     {
 
-        int[][] jaggedArrays = new int[3][];
+        /*int[][] jaggedArrays = new int[3][];
         jaggedArrays[0] = new int[] { 2, 5, 7, 8 };
         jaggedArrays[1] = new int[] { 3, 6, 7, 8, 10, 1 };
         jaggedArrays[2] = new int[] { 5, 10, 45, 67 };
@@ -29,6 +29,33 @@ public class JaggedArrays
 
             }
         }
+
+        */
+
+        int[] agesOfStudents = { 18, 14, 16, 13, 15, 17, 18, 16 };
+        int sumOfAges = 0;
+
+        // traverse the arrays 
+
+        for (int i = 0; i < agesOfStudents.Length; i++)
+        {
+            Console.WriteLine(agesOfStudents[i]);
+            sumOfAges += i;
+        }
+        // to find the sum of the ages 
+
+        foreach (var item in agesOfStudents)
+        {
+            sumOfAges += item;
+
+
+        }
+        Console.WriteLine("--------");
+        Console.WriteLine(sumOfAges);
+
+        //to find the average 
+
+
 
     }
 
