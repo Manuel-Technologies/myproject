72cce5b2cc75798e3fc161e1a13b08f5585147d3
Manuel Chekwubechukwu <emmanuelchekwubechukwu22@gmail.com>
2025-12-28 18:02:19 +0100

    Refactor PasswordManager class and passwordEntry class and structure

--- PasswordManger.cs
+++ PasswordManger.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Linq;
 
@@ -15,91 +14,104 @@ namespace sum;
 public class PasswordManager
 
 //static List<object> passwordDetails = new List<object>();
-static List<PasswordEntry> passwordDetails = new List<PasswordEntry>();
+
 
 {
-   // static List<object> passwordDetails = new List<object>();
+    static List<PasswordEntry> passwordDetails = new List<PasswordEntry>();
+    // static List<object> passwordDetails = new List<object>();
 
-public static void Main()
-{
-    string userChoice;
-    Console.WriteLine("welcome to your password manager");
-    Console.WriteLine();
-
-    Console.WriteLine("what do you want to do ? ");
-    Console.WriteLine("1. view passwords");
-    Console.WriteLine("2. add passwords");
-    Console.WriteLine("delete passwords");
-    Console.WriteLine("4. update password");
-    Console.WriteLine("5. exit the app");
-
-    userChoice = Console.ReadLine();
-
-    //using conditionals to determine the next action
-
-    if (userChoice == "1")
+    public static void Main()
     {
-        ViewPassword();
-    }
-
-    else if (userChoice == "2")
-    {
-        AddPasswordDetails();
-    }
-
-}
-
-public static void AddPasswordDetails()
-{
-    bool isRunning = true;
-
-    while (isRunning)
-    {
-        Console.WriteLine("input the password you want to store ");
-        string passwordToStore = Console.ReadLine();
-        Console.WriteLine("input the name of the account you want to store it to ");
-        string accountName = Console.ReadLine();
-
-        // passwordDetails.Add(passwordToStore);
-        //passwordDetails.Add(accountName);
-
-        Console.WriteLine("password stored successfully");
+        string userChoice;
+        Console.WriteLine("welcome to your password manager");
         Console.WriteLine();
 
-        Console.WriteLine("do you still want to store another password  ?  yes/no");
-        string loopDeterminant = Console.ReadLine();
+        Console.WriteLine("what do you want to do ? ");
+        Console.WriteLine("1. view passwords");
+        Console.WriteLine("2. add passwords");
+        Console.WriteLine("3. delete passwords");
+        Console.WriteLine("4. update password");
+        Console.WriteLine("5. exit the app");
 
-        if (loopDeterminant == "no")
+        userChoice = Console.ReadLine();
+
+        //using conditionals to determine the next action
+
+        if (userChoice == "1")
         {
-            //isRunning = false;
-            Main();
+            ViewPassword();
         }
 
-        else if (loopDeterminant == "yes")
+        else if (userChoice == "2")
         {
             AddPasswordDetails();
         }
 
-        else
-        {
-            Console.WriteLine("input a valid option");
-        }
     }
 
-}
-
-public static void ViewPassword()
-{
-    Console.WriteLine("this are your saved passwords");
-    Console.WriteLine("----------------------------------");
-
-    for (int i = 0; i < passwordDetails.Count; i++)
+    public static void AddPasswordDetails()
     {
-        //Console.WriteLine($"your {i + 1} password is {passwordDetails[i]}");
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine("input the password you want to store ");
+            string passwordToStore = Console.ReadLine();
+            Console.WriteLine("input the name of the account you want to store it to ");
+            string accountName = Console.ReadLine();
+
+            PasswordEntry entry = new PasswordEntry();
+            entry.AccountName = accountName;
+            entry.Password = passwordToStore;
+
+            passwordDetails.Add(entry);
+
+            // passwordDetails.Add(passwordToStore);
+            //passwordDetails.Add(accountName);
+
+            Console.WriteLine("password stored successfully");
+            Console.WriteLine();
+
+            Console.WriteLine("do you still want to store another password  ?  yes/no");
+            string loopDeterminant = Console.ReadLine();
+
+            if (loopDeterminant == "no")
+            {
+                //isRunning = false;
+                Main();
+            }
+
+            else if (loopDeterminant == "yes")
+            {
+                AddPasswordDetails();
+            }
+
+            else
+            {
+                Console.WriteLine("input a valid option");
+            }
+        }
 
     }
-    Console.WriteLine("_____________________");
-}
+
+    public static void ViewPassword()
+    {
+        Console.WriteLine("this are your saved passwords");
+        Console.WriteLine("----------------------------------");
+
+        for (int i = 0; i < passwordDetails.Count; i++)
+        {
+            //Console.WriteLine($"your {i + 1} password is {passwordDetails[i]}");
+
+            PasswordEntry entry = passwordDetails[i];
+
+            Console.WriteLine($"Account : {entry.AccountName}");
+            Console.WriteLine($"Password : {entry.Password}");
+            Console.WriteLine("");
+
+        }
+        Console.WriteLine("_____________________");
+    }
 
 
 
@@ -111,11 +123,8 @@ public class PasswordEntry
     public string AccountName { get; set; }
     public string Password { get; set; }
 
-    PasswordEntry entry = new PasswordEntry();
-    entry.AccountName=accountName;
-    entry.Password=passwordToStore;
-    
-    passwordDetails.Add(entry);
+
 
 }
+
 
