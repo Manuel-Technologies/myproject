3438cfff56f22bc4ce34f13a485294ccd6dacbc7
Manuel Chekwubechukwu <emmanuelchekwubechukwu22@gmail.com>
2025-12-29 00:52:06 +0100

    Refactor PasswordManager for clarity and functionality
    Removed commented-out code and improved password storage logic.

--- PasswordManger.cs
+++ PasswordManger.cs
@@ -3,22 +3,12 @@ using System.Linq;
 
 using System.Collections.Generic;
 using sum;
-
-
-
 namespace sum;
-//static List<object> passwordDetails = new List<object>();
-//static List<PasswordEntry> passwordDetails = new List<PasswordEntry>();
-
 
 public class PasswordManager
-
-//static List<object> passwordDetails = new List<object>();
-
-
 {
     static List<PasswordEntry> passwordDetails = new List<PasswordEntry>();
-    // static List<object> passwordDetails = new List<object>();
+
 
     public static void Main()
     {
@@ -59,16 +49,10 @@ public class PasswordManager
             string passwordToStore = Console.ReadLine();
             Console.WriteLine("input the name of the account you want to store it to ");
             string accountName = Console.ReadLine();
-
             PasswordEntry entry = new PasswordEntry();
             entry.AccountName = accountName;
             entry.Password = passwordToStore;
-
             passwordDetails.Add(entry);
-
-            // passwordDetails.Add(passwordToStore);
-            //passwordDetails.Add(accountName);
-
             Console.WriteLine("password stored successfully");
             Console.WriteLine();
 
@@ -89,6 +73,10 @@ public class PasswordManager
             else
             {
                 Console.WriteLine("input a valid option");
+                for (int i = 0; i < passwordDetails.Count; i++)
+                {
+                    Console.WriteLine($" {i + 1} account : {entry.AccountName}");
+                }
             }
         }
 
@@ -111,6 +99,38 @@ public class PasswordManager
 
         }
         Console.WriteLine("_____________________");
+
+
+        string userChoice;
+        //  Console.WriteLine("welcome to your password manager");
+        Console.WriteLine();
+
+        Console.WriteLine("what do you want to do ? ");
+        Console.WriteLine("1. view passwords");
+        Console.WriteLine("2. add passwords");
+        Console.WriteLine("3. delete passwords");
+        Console.WriteLine("4. update password");
+        Console.WriteLine("5. exit the app");
+
+        userChoice = Console.ReadLine();
+
+        //using conditionals to determine the next action
+
+        if (userChoice == "1")
+        {
+            ViewPassword();
+        }
+
+        else if (userChoice == "2")
+        {
+            AddPasswordDetails();
+        }
+    }
+
+    public static void UpdatePasswordDetails()
+    {
+        Console.WriteLine("choose the account ID that you want to change ");
+
     }
 
 
